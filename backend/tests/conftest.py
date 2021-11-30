949db94d1f676a39b41a27b59f2a7862180df383
Frank Casanova <frankcasanova.info@gmail.com>
2021-12-01 00:24:35 +0100

    first test done

---
+++ backend/tests/conftest.py
@@ -0,0 +1,75 @@
+from typing import Any
+from typing import Generator
+from db.session import get_db
+from db.base import Base
+from apis.base import api_router
+
+import pytest
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# this is to include backend dir in sys.path so that we can import from db,main.py
+
+
+def start_application() -> FastAPI:
+    app = FastAPI()
+    app.include_router(api_router)
+    return app
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
+                       "check_same_thread": False})
+
+# Use connect_args parameter only with sqlite
+SessionTesting = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+
+@pytest.fixture(scope="function")
+def app() -> Generator[FastAPI, Any, None]:
+    """
+    Create a fresh database on test case.
+    """
+    Base.metadata.create_all(engine)  # create the tables
+    _app = start_application()
+    yield _app
+    Base.metadata.drop_all(engine)  # drop the tables
+
+
+@pytest.fixture(scope="function")
+def db_session(app: FastAPI) -> Generator[SessionTesting, Any, None]:
+    """
+    Create a fresh database on test case.
+    """
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = SessionTesting(bind=connection)
+    yield session
+    session.close()
+    transaction.rollback()
+    connection.close()
+
+
+@pytest.fixture(scope="function")
+def client(
+    app: FastAPI, db_session: SessionTesting
+) -> Generator[TestClient, Any, None]:
+    """
+    Create a new FastAPI TestClient that uses the `db_session` fixture to override
+    the `get_db` dependency that is injected into routes.
+    """
+
+    def _get_test_db():
+        try:
+            yield db_session
+        finally:
+            pass
+
+    app.dependency_overrides[get_db] = _get_test_db
+    with TestClient(app) as client:
+        yield client
