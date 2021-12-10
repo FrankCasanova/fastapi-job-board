2b3647ec95a4aa5618b73030f2abbb03ee6a15fa
Frank Casanova <frankcasanova.info@gmail.com>
2021-12-10 19:21:44 +0100

    jwt authentication and authorization partially added

--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -1,5 +1,7 @@
 from typing import List
 
+from apis.version1.route_login import get_current_user_from_token
+from db.models.users import User
 from db.repository.jobs import create_new_job
 from db.repository.jobs import delete_job_by_id
 from db.repository.jobs import list_jobs
@@ -13,17 +15,19 @@ from schemas.jobs import JobCreate
 from schemas.jobs import ShowJob
 from sqlalchemy.orm import Session
 
-
 router = APIRouter()
 
 
 @router.post("/create-job/", response_model=ShowJob)
-def create_job(job: JobCreate, db: Session = Depends(get_db)):
+def create_job(
+    job: JobCreate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user_from_token),
+):
     """
     Create a new job
     """
-    current_user = 1
-    job = create_new_job(job=job, db=db, owner_id=current_user)
+    job = create_new_job(job=job, db=db, owner_id=current_user.id)
     return job
 
 
@@ -52,15 +56,25 @@ def read_jobs(db: Session = Depends(get_db)):
 
 
 @router.delete("/delete/{id}")
-def delete_job(id: int, db: Session = Depends(get_db)):
+def delete_job(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user_from_token),
+):
     """
     Delete a job by id
     """
-    current_user_id = 1
-    message = delete_job_by_id(id=id, db=db, owner_id=current_user_id)
-    if not message:
-        raise HTTPException(
+    job = retrieve_job(id=id, db=db)
+    if not job:
+        return HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Job with id: {id} does not exist.",
         )
-    return dict(msg="Successfully deleted job")
+    print(job.owner_id, current_user.id, current_user.is_superuser)
+    if job.owner_id == current_user.id or current_user.is_superuser:
+        delete_job_by_id(id=id, db=db, owner_id=current_user.id)
+        return {"message": f"Job with id: {id} deleted."}
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="You are not allowed to delete this job.",
+    )
--- backend/apis/version1/route_login.py
+++ backend/apis/version1/route_login.py
@@ -9,7 +9,10 @@ from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import HTTPException
 from fastapi import status
+from fastapi.security import OAuth2PasswordBearer
 from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
+from jose import JWTError
 from schemas.tokens import Token
 from sqlalchemy.orm import Session
 
@@ -42,3 +45,34 @@ def login_for_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
+
+# new function, It works as a dependency
+
+
+def get_current_user_from_token(
+    token: str = Depends(oauth2_scheme), Session=Depends(get_db)
+):
+    """
+    This function is used to get the current user from the token.
+
+    """
+    credentials_expetion = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_expetion
+    except JWTError:
+        raise credentials_expetion
+    user = get_user(username=username, db=Session)
+    if user is None:
+        raise credentials_expetion
+    return user
